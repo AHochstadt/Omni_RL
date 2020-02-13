5a5d7afa58c4f4717d976c11f045d3ee4ca89bd7
ahochstadt <andrew.hochstadt@gmail.com>
2020-02-13 15:56:34 -0500

    Added optional sec argument to main.py

--- global_vars.py
+++ global_vars.py
@@ -1,5 +1,20 @@
+import socket
+
+
+
 omni_dir = '/home/andrew/All_Trading/Studies/Omni_Project/'
 tdm_dir = '/media/andrew/FreeAgent Drive/Market_Data/Tick_Data_Manager/'
+if socket.gethostname() == 'andrew-ThinkPad-T450s':
+    print('\n\n', 'We are running on Andrews ThinkPad', '\n\n')
+elif socket.gethostname()[:5] == 'login':
+    print('\n\n', 'We are running on Intel DevCloud', '\n\n')
+    omni_dir = '/home/u37993/Omni_Project/'
+    tdm_dir = '/home/u37993/Omni_Project/'
+
+else:
+    print('\n\n', socket.gethostname(), 'hostname not recognized. We are going to assume to be running on Intel DevCloud', '\n\n')
+    omni_dir = '/home/u37993/Omni_Project/'
+    tdm_dir = '/home/u37993/Omni_Project/'
 
 day_chg_incs = [1, 3, 5] # number of days to calculate for day_cols
 minute_incs = [1, 5, 15, 30, 60] # number of minutes in the past to calculate for minute cols
@@ -7,6 +22,49 @@ minute_incs = [1, 5, 15, 30, 60] # number of minutes in the past to calculate fo
 action_size = 3 # HOLD, BUY, SELL
 
 keras_models = [
+
+
+    [{'layer': 'Dense', 'activation': 'relu', 'units': 256},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 128},
+     {'layer': 'Dropout', 'rate': 0.2},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dropout', 'rate': 0.2},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dropout', 'rate': 0.2},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dropout', 'rate': 0.2},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dense', 'activation': 'linear', 'units': action_size}],
+
+    [{'layer': 'Dense', 'activation': 'relu', 'units': 256},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 128},
+     {'layer': 'Dropout', 'rate': 0.2},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dropout', 'rate': 0.15},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dropout', 'rate': 0.1},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dropout', 'rate': 0.05},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dense', 'activation': 'linear', 'units': action_size}],
+
+    [{'layer': 'Dense', 'activation': 'relu', 'units': 256},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 128},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dense', 'activation': 'relu', 'units': 64},
+     {'layer': 'Dense', 'activation': 'linear', 'units': action_size}],
+
 
     [{'layer': 'Dense', 'activation': 'relu', 'units': 256},
      {'layer': 'Dense', 'activation': 'relu', 'units': 128},
--- main.py
+++ main.py
@@ -5,6 +5,17 @@ from train import autoTrain
 
 import pandas as pd
 import numpy as np
+import sys
+
+# sec = 'KELLOGG_CO'
+sec = 'US_Dollar_Index'
+
+print(sys.argv)
+if len(sys.argv)==1:
+    print('No sec argument, using', sec,'as default.')
+else:
+    sec = sys.argv[1]
+    print(sec, 'read as sec in main.py')
 
 # omni_dir = '/home/andrew/All_Trading/Studies/Omni_Project/'
 # # config_dir = omni_dir + 'Primary_Assets/US_Dollar_Index/Config1/'
@@ -12,9 +23,6 @@ import numpy as np
 regression_results_fn = tdm_dir+'regression_results.csv'
 data_summary_fn = tdm_dir+'data_summary.csv'
 data_summaryDF = pd.read_csv(data_summary_fn)
-
-# sec = 'KELLOGG_CO'
-sec = 'US_Dollar_Index'
 
 combDF = getCombDF(regression_results_fn, data_summary_fn)
 # combDF.to_csv(omni_dir+'combDF_temp.csv', index=False)
