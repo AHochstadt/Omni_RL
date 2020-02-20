2eeda67dffccdc0afebbc4abdd35a6b33240ed0c
ahochstadt <andrew.hochstadt@gmail.com>
2020-02-20 10:52:09 -0500

    Adds function to automatically select security to train, plus records a work log of securities trained

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
-from utils import in_notebook, utcToChi, getChiTimeNow, readCSV
+from utils import in_notebook, utcToChi, getChiTimeNow, readCSV, autoChooseSecurity
 from combinations import getCombDF
 from global_vars import omni_dir, tdm_dir, day_chg_incs, minute_incs, keras_models
 from train import autoTrain
 
 import pandas as pd
 import numpy as np
-import sys
+import sys, os
 
 # sec = 'KELLOGG_CO'
-sec = 'US_Dollar_Index'
+sec = 'NONE CHOSEN'
 
 print(sys.argv)
 if len(sys.argv)==1:
-    print('No sec argument, using', sec,'as default.')
+    print('No sec argument, autoChoosing.')
+    sec = autoChooseSecurity(omni_dir)
 else:
     sec = sys.argv[1]
     print(sec, 'read as sec in main.py')
@@ -27,5 +28,19 @@ data_summaryDF = pd.read_csv(data_summary_fn)
 combDF = getCombDF(regression_results_fn, data_summary_fn)
 # combDF.to_csv(omni_dir+'combDF_temp.csv', index=False)
 # combDF = readCSV(omni_dir+'combDF_temp.csv')
+
+# record our security choice to the work log
+work_log_fn = omni_dir + 'work_log.csv'
+work_logDF = pd.DataFrame(columns=['Sec', 'ChosenTime'])
+if not os.path.exists(work_log_fn):
+    print('work log doesnt exist. Creating a new one:', work_log_fn)
+else:
+    work_logDF = pd.read_csv(work_log_fn, parse_dates=['ChosenTime'])
+work_logDF = work_logDF.append({'Sec': sec, 'ChosenTime': pd.datetime.now()}, ignore_index=True)
+
+print('='*30, '\nsaving work_logDF to', work_log_fn)
+print(work_logDF.tail())
+
+work_logDF.to_csv(work_log_fn, index=False)
 
 autoTrain(sec, combDF, keras_models, nrows=50000)
--- utils.py
+++ utils.py
@@ -1,5 +1,7 @@
-import pytz
+import pytz, os
 import pandas as pd
+import numpy as np
+
 
 def in_notebook():
     try:
@@ -36,3 +38,54 @@ def readCSV(filename, nrows=None):
     DF = pd.read_csv(filename, nrows=nrows, parse_dates=date_cols+timestep_cols)
     for c in date_cols: DF[c] = DF[c].dt.date
     return DF
+
+def autoChooseSecurity(omni_dir = os.getcwd()):
+    """For now, let's just figure out if it's being worked on in another job, and how many models have been tried"""
+    print('in autoChooseSecurity with omni_dir =', omni_dir)
+    securities = os.listdir(omni_dir+'/Primary_Assets')
+    model_countDF = pd.DataFrame({'Sec': securities, 'ModelCount': 0})
+    for sec in securities:
+        sec_dir = omni_dir+'/Primary_Assets/'+sec+'/'
+        configs = [d for d in os.listdir(sec_dir) if d[:6]=='Config']
+        if len(configs) == 0:
+            print('No configs for', sec)
+        else:
+            config_nums = [int(cfg.split('Config')[1]) for cfg in configs]
+            max_config = str(max(config_nums))
+            config_dir = sec_dir + 'Config' + max_config + '/'
+            all_progress_summary_fn = config_dir + 'Deep_Models/all_progress_summary.csv'
+            if not os.path.exists(all_progress_summary_fn):
+                print('\nall_progress_summary_fn does not exist: \n', all_progress_summary_fn)
+            else:
+                all_progress_summaryDF = pd.read_csv(all_progress_summary_fn)
+                model_count = len(all_progress_summaryDF.Model.unique())
+                model_countDF.loc[model_countDF.Sec == sec, 'ModelCount'] = model_count
+
+    work_log_fn = omni_dir + 'work_log.csv'
+    time_now = pd.datetime.now()
+    num_hours_wait = 6
+
+    work_logDF = pd.DataFrame(columns=['Sec', 'ChosenTime'])
+    if not os.path.exists(work_log_fn):
+        print('work log doesnt exist. Creating a new one:', work_log_fn)
+    else:
+        work_logDF = pd.read_csv(work_log_fn, parse_dates=['ChosenTime'])
+        # [x] remove from model_countDF any security that's been worked on less than num_hours_wait ago
+        last_chosenDF = work_logDF.groupby('Sec').agg({'ChosenTime': 'max'})
+        last_chosenDF['Sec'] = last_chosenDF.index
+    #     secs_chosen_recently = last_chosenDF.Sec.loc[float((time_now - last_chosenDF.ChosenTime).seconds)/60/60 < num_hours_wait]
+        secs_chosen_recently = last_chosenDF.Sec.loc[(time_now - last_chosenDF.ChosenTime).astype('timedelta64[h]') < num_hours_wait]
+        secs_chosen_recently = secs_chosen_recently.values
+        print('secs_chosen_recently:', secs_chosen_recently)
+        model_countDF = model_countDF.loc[~model_countDF.Sec.isin(secs_chosen_recently)]
+    assert len(model_countDF) > 0, "We have no securities that have not been tried recently"
+    # Randomly choose a weighted choice in the bottom 30% of ModelCounts.
+    # This is to prevent a situation where one security is throwing errors
+    # for some reason and we keep trying it unsuccessfully.
+    threshold = model_countDF.ModelCount.quantile(.3)
+    model_count_subDF = model_countDF.loc[model_countDF.ModelCount <= threshold]
+    model_count_subDF['Weights'] = (threshold - model_countDF.ModelCount)+.1 #adds .1 because if we didn't then some weights would be 0
+    model_count_subDF.Weights /= model_count_subDF.Weights.sum()
+    chosen_security = np.random.choice(model_count_subDF.Sec.values, p=model_count_subDF.Weights.values)
+    print('\n\n\n', '='*50, '\nchosen_security:', chosen_security, '\n', '='*50, '\n\n\n')
+    return chosen_security
